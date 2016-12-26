b9b1ed968993bb03fb4743c768f8e6c45e38fe6b
DESKTOP-DB7PPLA\kill <zuolintong@gmail.com>
2016-12-26 15:02:41 +0800

    Revert "0.01"
    This reverts commit 1b11c7703c92ff7995c18752b9b94bd960fa2ffa.

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,83 +1,26 @@
 #include "stdafx.h"
 #include <iostream>
 #include <memory>
+
 using namespace std;
-void 
-test_swap();
-void 
-test_Isearch_Int();
-void
-test_Isearch_Char();
-void 
-*Isearch(void *key, void *base, int n, int elemSize, int (*cmpfn) (void *, void*));
-void 
-swap(void *ap, void *bp,int size);
-int 
-Int_Cmp(void *emel1, void *emel2);
-int
-Str_Cmp(void *emel1, void *emel2);
+
+void swa(void *ap, void *bp,int size);
 int main() {
-	//test_swap();
-	//test_Isearch_Int();
-	test_Isearch_Char();
-}
-void test_swap() {
 	char *a = "aaaaaa";
+	cout << &a << endl;
 	char *b = "bbbbbb";
-	swap(&a, &b, sizeof(char *));
+
+	swa(&a, &b,sizeof(char *));
+	cout << &a << endl;
+	cout << a << endl;
 	while (1);
 }
-void test_Isearch_Int() {
-	int a[] = { 1,2,3,4,5,6,7,8 };
-	int b = 3;
-	int n = 8;
-	int elemSize = sizeof(int);
-	int *found =(int *) Isearch(&b, &a, n, elemSize, Int_Cmp);
-	if (found == NULL) {
-		cout << "Not in array" << endl;
-	}
-	else {
-		cout << "In array" << endl;
-	}
-	while (1);
-}
-void test_Isearch_Char() {
-	char *notes[] = { "Ab","F#","B","Gb","D" };
-	char *fevourte = "Eb";
-	char **found = (char **)Isearch(&fevourte, notes, 5, sizeof(char*), Str_Cmp);
-	if (found == NULL) {
-		cout << "Not in array" << endl;
-	}
-	else {
-		cout << "In array" << endl;
-	}
-	while (1);
-}
-void swap(void *ap, void *bp, int size) {
+
+void swa(void *ap, void *bp, int size) {
 	char *buff;
 	buff = (char *)malloc(size);
 
 	memcpy(buff, ap,size);
 	memcpy(ap, bp, size);
 	memcpy(bp, buff, size);
-}
-void *Isearch(void *key, void *base, int n, int elemSize, int (*cmpfn) (void *, void*)) {
-	for (int i = 0; i < n; i++) {
-		void *ElemAddr = (char *)base + i*elemSize;
-		if (cmpfn(key, ElemAddr) == 0) {
-			return ElemAddr;
-		}
-	}
-	return NULL;
-}
-int Int_Cmp(void *emel1, void *emel2) {
-	int *ap1 = static_cast<int *>(emel1);
-	int *ap2 = static_cast<int *>(emel2);
-	return (*ap1 == *ap2);
-}
-int
-Str_Cmp(void *emel1, void *emel2) {
-	char *ap1 = *static_cast<char **>(emel2);
-	char *ap2 = *static_cast<char **>(emel2);
-	return strcmp(ap1,ap2);
 }
